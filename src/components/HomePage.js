0763dcbd73fe951590bc994a248b5671af41e2ab
Xavier Perez <xap5xap@gmail.com>
2017-05-22 18:05:39 -0500

    Home about

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router';
 
 const HomePage = () => {
   return (
@@ -15,18 +14,21 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div className="col-md-5 col-sm-12">
-                                <h1 className="text-center">About Creative Tim
+                                <h1 className="text-center">About thexap
                                 
                                 <small className="subtitle">Get to know us</small></h1>
                                 <hr></hr>
                                 <p>
-                                Creative Tim offers Bootstrap based design elements that speed up your development work. From plugins to complex kits, we have everything you need. 
+                                Thexap offers software development services. Specifically web sites, progressive web apps and hybrid mobile apps for iOS and Android.
                                 </p>
                                 <p>
-                                    We love the web and care deeply for how users interact with a digital product. The idea behind Creative Tim was creating a standard of things we actually need and would personally use inside our web projects. We power businesses and individuals to create better looking web projects around the world.
+                                   We use the latests technologies for our projects such as ReactJS, Angular2, Angular4, Ionic2, Ionic3 to deliver the best quality in the shortest time.
                                 </p>
                                 <p>
-                                We take great interest in how our customers use our resources and offer strong support and unlimited updates. We are constantly thinking about how to make our products intuitive, beautiful and extremely easy to understand, so feel free to tell us your thoughts!                                   
+                                We are committed to create beautiful easy-to-use applications based on your requirements. We are constantly innovating and learning new technologies to be able to advise and help you grow.
+                                </p>
+                                <p>
+                                    Feel free to contacts us, we’ll be happy to help you!
                                 </p>
 
                 </div>
@@ -35,37 +37,7 @@ const HomePage = () => {
     </div>
 <div className="space-30"></div>
 </div>
-   <div className="main">
-    <div className="container tim-container">
-        <div id="extras">
-            <div className="space"></div>
-            <div className="row">
-                <div className="col-md-7 col-md-offset-0 col-sm-10 col-sm-offset-1">
-                    <div className="text-center">
-                        <img src="assets/img/dulau.jpg" alt="Rounded Image" className="img-rounded img-responsive img-dog"></img>
-                    </div>
-                </div>
-                <div className="col-md-5 col-sm-12">
-                                <h1 className="text-center">About Creative Tim
-                                
-                                <small className="subtitle">Get to know us</small></h1>
-                                <hr></hr>
-                                <p>
-                                Creative Tim offers Bootstrap based design elements that speed up your development work. From plugins to complex kits, we have everything you need. 
-                                </p>
-                                <p>
-                                    We love the web and care deeply for how users interact with a digital product. The idea behind Creative Tim was creating a standard of things we actually need and would personally use inside our web projects. We power businesses and individuals to create better looking web projects around the world.
-                                </p>
-                                <p>
-                                We take great interest in how our customers use our resources and offer strong support and unlimited updates. We are constantly thinking about how to make our products intuitive, beautiful and extremely easy to understand, so feel free to tell us your thoughts!                                   
-                                </p>
-
-                </div>
-            </div>
-        </div>
-    </div>
-<div className="space-30"></div>
-</div>
+   
 </div>
 
   );
