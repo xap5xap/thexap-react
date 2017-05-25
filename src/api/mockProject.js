08a5c2c9e9a25372f4faf851c562ebde93f7f347
Xavier Perez <xap5xap@gmail.com>
2017-05-25 11:56:13 -0500

    Add images to firebase

--- src/api/mockProject.js
+++ src/api/mockProject.js
@@ -9,7 +9,7 @@ const projects = [
     name: 'Photo Inspiration',
     type: 'Mobile app',
     technology: [ "Angular2", "Ionic2", "Cordova", 'Flickr Api' ],
-    imgUrl: '/assets/img/PhotoInspirationSplash.jpg',
+    imgUrl: 'https://firebasestorage.googleapis.com/v0/b/xap5xap-1d8a9.appspot.com/o/thexap-images-public%2FPhotoInspirationSplash.jpg?alt=media&token=2f7715fe-ba09-4aae-8c5f-7205531e3cc0',
     visitUrl: 'https://play.google.com/store/apps/details?id=com.thexap.photoinspirationmobile'
   },
   {
@@ -17,7 +17,7 @@ const projects = [
     name: 'Multas de tránsito',
     type: 'Mobile app',
     technology: [ "Angular2", "Ionic2", "Cordova", 'Node JS' ],
-    imgUrl: '/assets/img/multas.jpg',
+    imgUrl: 'https://firebasestorage.googleapis.com/v0/b/xap5xap-1d8a9.appspot.com/o/thexap-images-public%2Fmultas.jpg?alt=media&token=9ab08165-62cd-440f-a3a6-2c2de6535ca8',
     visitUrl: 'https://play.google.com/store/apps/details?id=com.thexap.multasapp'
   },
   {
@@ -25,7 +25,7 @@ const projects = [
     name: 'thexap.com',
     type: 'Web site',
     technology: [ "React JS", 'Bootstrap' ],
-    imgUrl: '/assets/img/x_logo.jpg',
+    imgUrl: 'https://firebasestorage.googleapis.com/v0/b/xap5xap-1d8a9.appspot.com/o/thexap-images-public%2Fx_logo.jpg?alt=media&token=67407121-b27f-4736-b947-1335a9ff7410',
     visitUrl: 'https://thexap.com/'
   },
   {
@@ -33,7 +33,7 @@ const projects = [
     name: 'UR Money',
     type: 'Web site',
     technology: [ "Angular4", "Ionic3", "Firebase", 'Node JS' ],
-    imgUrl: '/assets/img/ur-logo.png',
+    imgUrl: 'https://firebasestorage.googleapis.com/v0/b/xap5xap-1d8a9.appspot.com/o/thexap-images-public%2Fur-logo.png?alt=media&token=2be6c43e-6c17-4d8a-b615-56456995d28f',
     visitUrl: 'https://web.ur.technology/r/A9wYEB'
   },
   {
@@ -41,7 +41,7 @@ const projects = [
     name: 'City knots',
     type: 'Mobile app',
     technology: [ "Angular4", "Ionic3", "Cordova"],
-    imgUrl: '/assets/img/666.jpg',
+    imgUrl: 'https://firebasestorage.googleapis.com/v0/b/xap5xap-1d8a9.appspot.com/o/thexap-images-public%2F666.jpg?alt=media&token=b49a3270-6729-4536-b62a-5b9a054c31e2',
     visitUrl: 'https://cityknots.com/'
   }
 ];
